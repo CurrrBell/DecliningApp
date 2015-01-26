f2fbe5ed2c7b354e941572ea591e16483f2db7e9
Chris Bell <chrislbellny@gmail.com>
2015-01-25 22:39:26 -0500

    Created Skeleton
    Created multiple view controller files for each planned view in app.
    All are currently empty. There are not corresponding storyboard elements

---
+++ Declining App/Declining App/BuyOrSell.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Declining App
+//
+//  Created by Bell, Christopher on 1/25/15.
+//  Copyright (c) 2015 Bell, Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Declining App/Declining App/SellerSettings.swift
@@ -0,0 +1,35 @@
+//
+//  SellerSettings.swift
+//  Declining App
+//
+//  Created by Bell, Christopher on 1/25/15.
+//  Copyright (c) 2015 Bell, Christopher. All rights reserved.
+//
+
+import UIKit
+
+class SellerSettings: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
